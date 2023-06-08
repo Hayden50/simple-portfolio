48f140a3ac90c9acfd084ca2462acd5b7a7baeba
Hayden50 <haydenrieder50@hotmail.com>
2023-06-08 11:32:04 -0400

    Added my info and work section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('my-projects-link')) {
+  document.getElementById('my-projects-link').addEventListener('click', () => {
+    document.getElementById('my-projects-section').scrollIntoView({behavior: "smooth"})
+  })
+}
